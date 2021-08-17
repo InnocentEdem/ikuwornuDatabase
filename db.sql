76d4f14abd01f503cf3a1bb841448f621f575c1c
InnocentEdem <82113948+InnocentEdem@users.noreply.github.com>
2021-08-17 05:16:55 +0000

    Update ERD

--- db.sql
+++ db.sql
@@ -2,8 +2,10 @@ create table quizmasters(
     id serial primary key,
     fname varchar(100) not null,
     lname varchar(100) not null,
-    password varchar(100)not null,
-    isAdmin boolean
+    user_password varchar(100)not null,
+    isAdmin boolean,
+    username varchar(15) not null,
+    UNIQUE(username)
 );
 
 create table questions(
@@ -17,17 +19,11 @@ create table questions(
 create table quiz_sessions(
     session_id serial primary key,
     date_created date,
-    max_question_point int,
-    min_question_point int,
-    seconds_per_question int,
-    number_of_questions int,
     quizmaster_id int references quizmasters(id)
 );
 create table contestants(
     id serial primary key,
     fname varchar(100),
     lname varchar(100),
-    score int,
     quiz_session_id int references quiz_sessions(session_id)
 );
-
